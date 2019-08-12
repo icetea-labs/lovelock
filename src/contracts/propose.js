e41ba68e3ff417a93a028596b62c0ffafa299ef9
hoanghuy226 <hoanghuy226@gmail.com>
2019-08-12 20:22:32 +0700

    add contract

--- src/contracts/propose.js
+++ src/contracts/propose.js
@@ -4,14 +4,15 @@ const { expect } = require(";");
   @view @state propose = [];
   @view @state addressToPropose = {};
 
-  @transaction createPropose(s_content: string, receiver: string, s_moreInfo: string) {
+  //info { img:Array, location:string, date:string }
+  @transaction createPropose(s_content: string, receiver: string, info: string) {
     const sender = msg.sender
-    // status: 0 = memory, 1 = propose, 2: reject
-    let menory = { sender, s_content, receiver, status: 0, moreInfo }
+    // status: pending: 0, accept_propose: 1, cancel_propose: 2
+    let pendingPropose = { sender, s_content, receiver, status: 0, info }
 
     //new memories
     const x = this.propose
-    const index = x.push(menory) - 1
+    const index = x.push(pendingPropose) - 1
     this.propose = x
 
     // new map address to propose
@@ -21,19 +22,37 @@ const { expect } = require(";");
     this.addressToPropose = y
 
     //emit Event
-    menory = Object.assign({}, menory, { index })
-    this.emitEvent("createPropose", { by: sender, menory }, ["by"]);
+    pendingPropose = Object.assign({}, pendingPropose, { index })
+    this.emitEvent("createPropose", { by: sender, pendingPropose }, ["by"]);
   }
 
-  @transaction confirmPropose(index: number, r_content: string, status: number, r_moreInfo: string) {
-    const sender = msg.sender
+  @transaction acceptPropose(index: number, r_content: string) {
+    this.confirmPropose(index, r_content, 1)
+  }
+
+  @transaction cancelPropose(index: number, r_content: string) {
+    this.confirmPropose(index, r_content, 2)
+  }
+  //private function
+  confirmPropose(index: number, r_content: string, status: number) {
     let propose = this.propose[index]
-    expect(sender !== propose.receiver, "User must be owner propose.")
+    // status: pending: 0, accept_propose: 1, cancel_propose: 2
+    if (status === 1) {
+      expect(msg.sender === propose.receiver, "Can't accept propose. You must be receiver.")
+    } else {
+      expect(msg.sender === propose.receiver || msg.sender === propose.sender, "You can't update propose. You must be owner propose.")
+    }
     propose = Object.assign({}, propose, { r_content, status })
     this.propose[index] = propose
 
     //emit Event
-    this.emitEvent("confirmPropose", { by: sender, propose }, ["by"]);
+    this.emitEvent("confirmPropose", { by: msg.sender, propose }, ["by"]);
   }
-
+  // Change info { img:Array, location:string, date:string }
+  @transaction changeInfoPromise(index: number, info: string) {
+    let propose = this.propose[index]
+    expect(msg.sender === propose.receiver || msg.sender === propose.sender, "Can't change info promise. You must be owner propose.")
+    propose = Object.assign({}, propose, { info })
+    this.propose[index] = propose
+  }
 }
