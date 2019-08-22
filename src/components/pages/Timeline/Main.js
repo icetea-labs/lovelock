ef705ec3c911bc4074120f4f791752c96f7dfe2a
LuongHV <softkiss243@gmail.com>
2019-08-22 11:40:11 +0700

    addTimeLine

--- src/components/pages/Timeline/Main.js
+++ src/components/pages/Timeline/Main.js
@@ -1,14 +1,176 @@
 import React, { PureComponent } from 'react';
 import styled from 'styled-components';
 import { connect } from 'react-redux';
+import InputBase from '@material-ui/core/InputBase';
 
 import { FlexBox, FlexWidthBox, rem } from '../../elements/Common';
 import Icon from '../../elements/Icon';
+
+const BannerContainer = styled.div`
+  margin-bottom: ${rem(20)};
+`;
+const ShadowBox = styled.div`
+  padding: 30px;
+  border-radius: 10px;
+  /* margin-bottom: 20px; */
+  background: #ffffff;
+  box-shadow: 0 4px 12px 0 rgba(0, 0, 0, 0.05);
+`;
+
+const LeftBox = styled.div`
+  width: 100%;
+  min-height: ${rem(360)};
+  i {
+    padding: 0 5px;
+  }
+  .btn_add_promise {
+    width: 172px;
+    height: 46px;
+    background: #ffffff;
+    border-radius: 23px;
+    font-weight: 600;
+    color: #8250c8;
+    border: 1px solid #8250c8;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 30px;
+  }
+  .title {
+    color: #141927;
+    font-weight: 600;
+    font-size: ${rem(14)};
+    text-transform: uppercase;
+    margin-bottom: ${rem(20)};
+  }
+`;
+const RightBox = styled.div`
+  width: 100%;
+  min-height: ${rem(360)};
+  box-sizing: border-box;
+  padding-left: ${rem(30)};
+  .fl {
+    float: left;
+  }
+  .fr {
+    float: right;
+  }
+  .post_container {
+    display: flex;
+    width: 100%;
+    .user_avatar {
+      width: 58px;
+      height: 58px;
+      border-radius: 10px;
+      overflow: hidden;
+      margin-right: ${rem(10)};
+    }
+    .post_input {
+      width: 100%;
+      height: 50px;
+      display: flex;
+      align-items: center;
+      .contentEditable {
+        width: 100%;
+        height: 19px;
+        font-family: Montserrat;
+        font-size: 16px;
+        font-weight: 500;
+        font-style: normal;
+        font-stretch: normal;
+        line-height: normal;
+        letter-spacing: normal;
+        color: #8f8f8f;
+        outline: none;
+        font-size: ${rem(16)};
+      }
+    }
+  }
+  .action {
+    width: 100%;
+    margin: 16px 0 16px;
+    display: inline-block;
+    .privacy {
+      display: inline-block;
+      float: left;
+    }
+    button {
+      width: 254px;
+      line-height: 46px;
+      float: right;
+      font-size: 16px;
+      color: #ffffff;
+      font-weight: 600;
+      border-radius: 23px;
+      box-shadow: 0 5px 14px 0 rgba(0, 0, 0, 0.06);
+      background-image: -webkit-linear-gradient(118deg, #b276ff, #fe8dc3);
+      background-image: linear-gradient(332deg, #b276ff, #fe8dc3);
+    }
+    .btn_post_policy {
+      width: 102px;
+      height: 36px;
+      border-radius: 21px;
+      background: #ffffff;
+      border: 1px solid #8250c8;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-family: Montserrat;
+      font-size: 12px;
+      font-weight: 500;
+      font-style: normal;
+      font-stretch: normal;
+      line-height: normal;
+      letter-spacing: normal;
+      color: #8f36b3;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      padding: 0 10px;
+    }
+  }
+`;
+const WarrperAcceptedPromise = styled.div`
+  display: flex;
+  align-items: flex-start;
+  margin-bottom: ${rem(20)};
+  :hover {
+    cursor: pointer;
+  }
+  .icon {
+    width: ${rem(36)};
+    height: ${rem(36)};
+    margin-right: ${rem(10)};
+    border-radius: 50%;
+    overflow: hidden;
+  }
+  .name {
+    color: #5a5e67;
+  }
+  .nick {
+    color: #8250c8;
+    font-size: ${rem(12)};
+  }
+`;
+const TagBox = styled.div`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  .tagName {
+    color: #8250c8;
+    margin-right: ${rem(7)};
+    font-size: ${rem(12)};
+    :hover {
+      cursor: pointer;
+    }
+  }
+`;
 
 class Main extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
+      tag: ['love', 'travel', 'honeymoon', 'relax', 'sweet'],
       ownerTag: ['honeymoon', 'travel'],
       isPromise: false,
       isPendingPromise: false,
@@ -25,6 +187,17 @@ class Main extends PureComponent {
     };
   }
 
+  renderTag = tag => {
+    // const { tag } = this.state;
+    return tag.map((item, index) => {
+      return (
+        <span className="tagName" key={index}>
+          #{item}
+        </span>
+      );
+    });
+  };
+
   render() {
     const {
       tag,
@@ -40,10 +213,9 @@ class Main extends PureComponent {
       memoryContent,
     } = this.state;
     return (
-      <BannerContainer>
-          <ShadowBox>
-            <TopContrainer proIndex={proIndex} />
-          </ShadowBox>
+      <React.Fragment>
+        <BannerContainer>
+          <ShadowBox>{/* <TopContrainer proIndex={proIndex} /> */}</ShadowBox>
         </BannerContainer>
 
         <FlexBox wrap="wrap">
@@ -56,16 +228,14 @@ class Main extends PureComponent {
                 </button>
                 <div className="title">Accepted promise</div>
                 <div>
-                  <PromiseLeftAccept
+                  {/* <PromiseLeftAccept
                     propose={propose}
                     address={address}
                     handlerSelectPropose={this.handlerSelectPropose}
-                  />
+                  /> */}
                 </div>
                 <div className="title">Pending promise</div>
-                <div>
-                  <PromiseLeftPending address={address} openPendingPromise={this.openPending} />
-                </div>
+                <div>{/* <PromiseLeftPending address={address} openPendingPromise={this.openPending} /> */}</div>
                 <div className="title">Popular Tag</div>
                 <TagBox>{this.renderTag(tag)}</TagBox>
               </ShadowBox>
@@ -90,7 +260,7 @@ class Main extends PureComponent {
                     </div>
                   </div>
                 </div>
-                <CustomPost avatarShow onChange={this.onChangeCus} />
+                {/* <CustomPost avatarShow onChange={this.onChangeCus} /> */}
               </div>
 
               <div className="action">
@@ -127,11 +297,11 @@ class Main extends PureComponent {
                   Share
                 </button>
               </div>
-              <MessageHistory />
+              {/* <MessageHistory /> */}
             </RightBox>
           </FlexWidthBox>
         </FlexBox>
-        {isPromise && <Promise close={this.closePromise} />}
+        {/* {isPromise && <Promise close={this.closePromise} />}
         {isPendingPromise && (
           <PromiseAlert
             propose={propose}
@@ -143,7 +313,8 @@ class Main extends PureComponent {
           />
         )}
         {isAccept && <PromiseConfirm close={this.closeConfirm} index={pendingIndex} />}
-        {isDeny && <PromiseConfirm isDeny close={this.closeConfirm} index={pendingIndex} />}
+        {isDeny && <PromiseConfirm isDeny close={this.closeConfirm} index={pendingIndex} />} */}
+      </React.Fragment>
     );
   }
 }
