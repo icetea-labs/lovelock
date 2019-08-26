250a1c263232541e1a68e6866e136fb8473ba5d8
hoanghuy226 <hoanghuy226@gmail.com>
2019-08-26 10:58:52 +0700

    add font

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './assets/sass/common.scss';
 import { connect } from 'react-redux';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { HomeLayout, Layout } from './components/layout/Layout';
