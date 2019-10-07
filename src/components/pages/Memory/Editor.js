81fc9a44dec59bc50ef65991894b82c578a14436
Tran Bao Huy <huy23121994@gmail.com>
2019-10-07 11:07:44 +0700

    update max-width for editor content

--- src/components/pages/Memory/Editor.js
+++ src/components/pages/Memory/Editor.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import Dante from 'Dante2';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+	wrapper: {
+		margin: '0 auto',
+		maxWidth: 740
+	}
+}));
 
 export default function Editor(props) {
+	const classes = useStyles();
 	
 	return (
-		<Dante
-			content={props.initContent ? props.initContent : false}
-			read_only={props.read_only ? true : false}
-			onChange={(Editor) => {
-				if (props.onChange) {
-					props.onChange(Editor.save.editorContent)
-				}
-			}}
-		/>
+		<div className={classes.wrapper}>
+			<Dante
+				content={props.initContent ? props.initContent : false}
+				read_only={props.read_only ? true : false}
+				onChange={(Editor) => {
+					if (props.onChange) {
+						props.onChange(Editor.save.editorContent)
+					}
+				}}
+			/>
+		</div>
 	);
 }
