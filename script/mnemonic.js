478af59bbb2cc40db31892d5ab888e21062e2429
Truong Hong Thi <thi1981@gmail.com>
2019-10-16 02:11:29 +0700

    Update seed script

--- script/mnemonic.js
+++ script/mnemonic.js
@@ -2,27 +2,24 @@ const bip39 = require('bip39')
 const HDKey = require('hdkey')
 const { codec, ecc } = require('@iceteachain/common')
 
-const path = 'm’/44’/349’/0’/0'
-
-const getPkey = (mnemonic, index) => {
-    const seed = bip39.mnemonicToSeedSync(mnemonic);
-    const hdkey = HDKey.fromMasterSeed(seed);
-    return hdkey.derive(path + index);
-}
-
-exports.toPkey = seed => {
-    if (!bip39.validateMnemonic(seed)) {
+exports.toPkey = mnemonic => {
+    if (!bip39.validateMnemonic(mnemonic)) {
         throw new Error('Wrong mnemonic format.');
     }
 
-    let index = 0, address, pkey
+    const seed = bip39.mnemonicToSeedSync(mnemonic);
+    const hdkey = HDKey.fromMasterSeed(seed).derive('m’/44’/349’/0’/0')
 
-    do {
-        pkey = getPkey(seed, index);
-        address = ecc.toPubKeyAndAddress(privateKey).address
-        index++
-    } while (!codec.isRegularAddress(address));
+    let pkey, found
+    for (let i = 0; !found; i++) {
+        if (i > 100) {
+            // there must be something wrong, because the ratio of regular account is 50%
+            throw new Error('Too many tries deriving regular account from seed.')
+        }
+        pkey = hdkey.deriveChild(i)
+        const { address } = ecc.toPubKeyAndAddress(privateKey)
+        found = codec.isRegularAddress(address)
+    }
 
     return pkey
-
 }
--- script/seed.js
+++ script/seed.js
@@ -2,40 +2,55 @@ const envfile = require('envfile')
 const fs = require('fs')
 const { toPkey } = require('./mnemonic')
 const { IceteaWeb3 } = require('@iceteachain/web3')
-
 const { mode, envPath } = require('./mode')
+const { randomBytes } = require('crypto')
 
 if (['-h', '--help'].includes(mode)) {
   console.log('Purpose: Seed data.')
   console.log('---------------')
-  console.log('deploy         seed data using .env file')
-  console.log('deploy prod    seed data using .env.production file')
-  console.log('deploy dev     seed data using .env.development file')
-  console.log('deploy [file]  deploy contract using [file]')
+  console.log('seed         seed data using .env file')
+  console.log('seed prod    seed data using .env.production file')
+  console.log('seed dev     seed data using .env.development file')
+  console.log('seed [file]  seed data using [file]')
   return
 }
 
 // load config
 console.log(`Load RPC endpoint from ${envPath}`)
-const { REACT_APP_RPC, REACT_APP_CONTRACT, SEED_PKEY, SEED_MNEMONIC } = envfile.parseFileSync(envPath)
-const tweb3 = new IceteaWeb3(REACT_APP_RPC)
-console.log(`Connected to ${REACT_APP_RPC}.`)
+const { 
+    REACT_APP_RPC, 
+    REACT_APP_CONTRACT, 
+    SEED_PKEY, 
+    SEED_MNEMONIC,
+    MEMORY_COUNT
+ } = envfile.parseFileSync(envPath)
+
+const toHttpRpc = wsUrl => {
+    const url = new URL(wsUrl)
+    return `http://${url.host}`
+}
+
+const endpoint = toHttpRpc(REACT_APP_RPC)
+const tweb3 = new IceteaWeb3(endpoint, { keepConnection: true })
+console.log(`Connect to ${endpoint}.`)
 
 const alias = tweb3.contract('system.alias').methods
 const did = tweb3.contract('system.did').methods
 const lovelock = tweb3.contract(REACT_APP_CONTRACT).methods
 
-const makeAlias = (account, prefix) => {
-    const from = account.address || account
-    const username = prefix + Date.now() + String(Math.random()).replace('.', '')
-    return alias.register(username, from)
+const makeAliasPair = (sender, receiver) => {
+    const bytes = randomBytes(8)
+    const senderName = 's_' + bytes.readUInt32BE(0).toString(36)
+    const receiverName = 'r_' + bytes.readUInt32BE(4).toString(36)
+    const register = (username, from) => alias.register(username, from)
         .sendCommit({ from })
         .then(({ returnValue }) => returnValue.split('.')[1])
+
+    return Promise.all([register(senderName, sender), register(receiverName, receiver)])
 }
 
-const makeLove = (m, account, ...args) => {
-    const from = account.address || account
-    return lovelock[m](...args).sendCommit( { from }).then(({ returnValue }) => returnValue)
+const makeLove = ({ method, from, send = 'sendCommit'}, ...args) => {
+    return lovelock[method](...args)[send]( { from }).then(({ returnValue }) => returnValue)
 }
 
 let fromAccount
@@ -55,41 +70,59 @@ const { address: toAccount } = tweb3.wallet.createRegularAccount()
 
 ;(async () => {
 
-    // register alias for from and to
-    const p1 = makeAlias(fromAccount, 'sender_')
-    const p2 = makeAlias(toAccount, 'receiver_')
-
-    const [ sender, receiver ]  = await Promise.all([p1, p2])
-  
-    console.log(sender, receiver)
+    const [ sender, receiver ]  = await makeAliasPair(fromAccount, toAccount)
+    console.log('sender: ' + sender)
+    console.log('receiver: ' + receiver)
 
     // create the lock
-    const pIndex = await makeLove('createPropose', fromAccount, `Create lock from ${sender} to ${receiver}`, toAccount, {
+    const pIndex = await makeLove({
+        method: 'createPropose', 
+        from: fromAccount
+    }, `Create lock from ${sender} to ${receiver}`, toAccount, {
         hash: ['QmcPqy322d4hzGKHY7etwm57dx3SWCE2mQRxFZ2LqtKvJc']
     })
     console.log('Lock index: ' + pIndex)
 
     // now, accept it
-    await makeLove('acceptPropose', toAccount, pIndex, `${receiver} accepts ${sender}`)
+    await makeLove({ 
+        method: 'acceptPropose', 
+        from: toAccount
+    }, pIndex, `${receiver} accepts ${sender}`)
 
     // then add 100 memories
-    const count = 10
+    const count = Number(MEMORY_COUNT || process.env.MEMORY_COUNT) || 50
+
     const promises = []
     const accounts = [fromAccount, toAccount]
     for (let i = 0; i < count; i++) {
         const from = accounts[i % 2]
-        promises.push(makeLove('addMemory', from, pIndex, false, `Memory ${i} for lock ${pIndex}`, {
+        promises.push(makeLove({
+            method: 'addMemory',
+            from,
+            send: 'sendAsync'
+        }, pIndex, false, `Memory ${i} for lock ${pIndex}`, {
             hash: ['QmeoAAKbzsvL6qvajiiRJcCNpNoKzGmYyPXcSxHcktbJbU'],
             date: Date.now()
         }).then(r => {
-                console.log(`Created memory ${i} for lock ${pIndex}`)
+                //console.log(`Created memory ${i} for lock ${pIndex}`)
                 return r
             }))
     }
 
+    // this would fail if more than 100 connection
     await Promise.all(promises)
+    // so we need to slice into chunk
+    // const chunksize = 90
+    // let result = []
+    // for (let i = 0; i < promises.length; i += chunksize) {
+    //     const chunk = promises.slice(i, i + chunksize);
+    //     result = result.concat(await Promise.all(chunk).then(r => {
+    //         console.log(`Memory inserted ${i} ~ ${i + chunk.length}`)
+    //         return r
+    //         //return new Promise((resolve, reject) => setTimeout(() => resolve(r), 0))
+    //     }))
+    // }
 
-    process.exit(0)
   
   })().catch(e => {
       console.error(e)
