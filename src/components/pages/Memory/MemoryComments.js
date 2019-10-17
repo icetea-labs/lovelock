fc60cfbb68e7393693479fcdfe6e1092ccbc316e
LuongHV <softkiss243@gmail.com>
2019-10-17 11:39:14 +0700

    Comment enhancement #225 change icon

--- src/components/pages/Memory/MemoryComments.js
+++ src/components/pages/Memory/MemoryComments.js
@@ -91,7 +91,7 @@ const StyledCardActions = withStyles(theme => ({
 }))(CardActions);
 const numComment = 4;
 export default function MemoryComments(props) {
-  const { handerNumberComment, memoryIndex, autoFc } = props;
+  const { handerNumberComment, memoryIndex, textInput } = props;
   const dispatch = useDispatch();
 
   // const privateKey = useSelector(state => state.account.privateKey);
@@ -238,7 +238,7 @@ export default function MemoryComments(props) {
                     notchedOutline: classes.notchedOutline,
                   },
                 }}
-                autoFocus={autoFc}
+                inputRef={textInput}
               />
             </Grid>
           </Grid>
